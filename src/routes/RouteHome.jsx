2013e4f022e1facded78ea018c51823900d23acd
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2024-01-02 16:45:04 +0200

    Update RouteHome.jsx

--- src/routes/RouteHome.jsx
+++ src/routes/RouteHome.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import HomeComponent from "../components/HomeComponent/HomeComponent";
 import CapitalFormComponent from "../components/CapitalFormComponent/CapitalFormComponent";
 
-// import useCountries from "../hooks/useCountries";
+import useCountries from "../hooks/useCountries";
 // import CountriesContext from "../contexts/CountriesContext/CountriesContext";
 export default function RouteHome() {
-  // let { state, handleCountryChange } = useCountries();
+  let { state, handleCountryChange } = useCountries();
   return (
     <>
       <HomeComponent />
